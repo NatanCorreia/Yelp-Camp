573611d8884255f266692411282d2b2ec4172ce3
NatanCorreia <natansoarescorreia@gmail.com>
2022-04-05 16:20:09 -0300

    fixing reviews

--- models/review.js
+++ models/review.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const reviewSchema = new Schema({
-    text: String,
+    body: String,
     rating: Number,
     author:{
         type: Schema.Types.ObjectId,
